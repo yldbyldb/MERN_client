8ad3eee5838f65fa1d1461ec35983db406dbd4d8
Bruce Dong <bruce.dong@societyone.com.au>
2022-04-18 21:43:07 +1000

    add a commit for .map() in reducers

--- src/reducers/posts.js
+++ src/reducers/posts.js
@@ -1,3 +1,4 @@
+// eslint-disable-next-line import/no-anonymous-default-export
 export default (posts = [], action) => {
 	switch (action.type) {
 		case 'FETCH_ALL':
@@ -5,7 +6,7 @@ export default (posts = [], action) => {
 		case 'CREATE':
 			return [...posts, action.payload]
 		case 'UPDATE':
-			return posts.map(post => post._id === action.payload._id ? action.payload : post)
+			return posts.map(post => post._id === action.payload._id ? action.payload : post) //由于这里数组的中的元素是引用数据类型，不是基本数据类型，所以会改变元素
 		default:
 			return posts
 	}
