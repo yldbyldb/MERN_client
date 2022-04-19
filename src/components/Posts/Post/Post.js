aeeddbd8f0bbe7eb227dbd15440d5e29c22d68cd
Bruce Dong <bruce.dong@societyone.com.au>
2022-04-19 17:34:00 +1000

    fixed the like function

--- src/components/Posts/Post/Post.js
+++ src/components/Posts/Post/Post.js
@@ -4,11 +4,15 @@ import ThumbUpAltIcon from '@material-ui/icons/ThumbUpAlt'
 import DeleteIcon from '@material-ui/icons/Delete'
 import MoreHorizIcon from '@material-ui/icons/MoreHoriz'
 import moment from 'moment'
+import { useDispatch } from 'react-redux'
+import { deletePost, likePost } from '../../../actions/posts'
 // import useStyles from './styles'
 
 const Post = ({ post, setCurrentId }) => {
 	// const classes = useStyles()
 	console.log(post)
+	const dispatch = useDispatch()
+
 	return (
 		<Card>
 			<CardMedia image={post.selectedFiles} title={post.title} />
@@ -29,12 +33,12 @@ const Post = ({ post, setCurrentId }) => {
 				<Typography variant='h5' gutterBottom>{post.message}</Typography>
 			</CardContent>
 			<CardActions>
-				<Button size='small' color='primary' onClick={() => { }}>
+				<Button size='small' color='primary' onClick={() => dispatch(likePost(post._id))}>
 					<ThumbUpAltIcon fontSize='small' />
 					Like
 					{post.likeCount}
 				</Button>
-				<Button size='small' color='primary' onClick={() => { }}>
+				<Button size='small' color='primary' onClick={() => dispatch(deletePost(post._id))}>
 					<DeleteIcon fontSize='small' />
 					Delete
 				</Button>
--- src/reducers/posts.js
+++ src/reducers/posts.js
@@ -6,7 +6,10 @@ export default (posts = [], action) => {
 		case 'CREATE':
 			return [...posts, action.payload]
 		case 'UPDATE':
+		case 'LIKE':
 			return posts.map(post => post._id === action.payload._id ? action.payload : post) //由于这里数组的中的元素是引用数据类型，不是基本数据类型，所以会改变元素
+		case 'DELETE': 
+			return posts.filter(post => post._id !== action.payload)
 		default:
 			return posts
 	}
