a6e2106dc3a26bc3ce0ed2ae57d8619a991a5269
yldbyldb@gmail.com <brucedong@BRUCEdeMacBook-Pro.local>
2022-05-26 23:27:39 +1000

    fixed the Link from react-router-dom

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom'
 import { Provider } from 'react-redux'
 import { createStore, applyMiddleware, compose } from 'redux'
 import thunk from 'redux-thunk'
+import { BrowserRouter } from 'react-router-dom';
 
 import reducers from './reducers'
 import App from './App'
@@ -16,7 +17,9 @@ const store = createStore(reducers, enhancer)
 
 ReactDOM.render(
     <Provider store={store}>
-        <App /> 
+        <BrowserRouter>
+            <App /> 
+        </BrowserRouter>
     </Provider>,
     document.getElementById('root')
 )
