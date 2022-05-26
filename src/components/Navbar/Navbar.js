418342821e47cb113a45faeb49ff92832b38a71c
Bruce Dong <bruce.dong@societyone.com.au>
2022-05-26 22:12:00 +1000

    add react-router-dom

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import { AppBar, Typography } from '@material-ui/core'
+import { Link } from 'react-router-dom'
 
 import memories from '../../images/memories.png'
 import useStyles from './styles'
@@ -9,7 +10,7 @@ const Navbar = () => {
 
   return (
     <AppBar className={classes.appBar} position='static' color='inherit'>
-      <Typography className={classes.heading} variant='h2' align='center'>Memories</Typography>
+      <Typography className={classes.heading} component={Link} to="/" variant='h2' align='center'>Memories</Typography>
       <img className={classes.image} src={memories} alt="memories" height=
     '60' />
 </AppBar>  )
